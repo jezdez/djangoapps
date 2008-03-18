609c69bfcc67ccadd344087fc02564d654ccc61a
sebastian.hillig <sebastian.hillig@2c1cd5b0-8548-0410-a813-dff95423cbc0>
2008-03-18 23:20:40 +0000

    Corrected stupid error

--- trunk/djangoapps/templatetags/helpertags.py
+++ trunk/djangoapps/templatetags/helpertags.py
@@ -4,11 +4,11 @@ register = template.Library()
 
 def below(value, arg):
     "Checks whether value is below args"
-    return arg < value
+    return arg > value
 
 def above(value, arg):
     "Checks whether value is above args"
-    return arg > value
+    return arg < value
     
 register.filter('below', below)
 register.filter('above', above)
