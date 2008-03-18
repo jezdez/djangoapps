6e280670c3242081f42dcd88b59aab6752b12c5c
thauber <thauber@2c1cd5b0-8548-0410-a813-dff95423cbc0>
2008-03-18 22:26:32 +0000

    added forms

--- trunk/djangoapps/models.py
+++ trunk/djangoapps/models.py
@@ -2,14 +2,8 @@ from django.db import models
 from django.template import defaultfilters
 from django.utils.translation import ugettext_lazy as _
 from django.core.urlresolvers import reverse
+from django.contrib.auth.models import User
 import datetime
-
-class Author(models.Model):
-    name = models.CharField(max_length=128, unique=True, verbose_name=_("Name"))
-    slug = models.SlugField(max_length=128, unique=True, verbose_name=_("Slug"))
-    email = models.EmailField(blank=True)
-    class Admin:
-        pass
 
 class DjangoApp(models.Model):
     PYPI, GOOGLE, SOURCEFORGE = 1, 2, 3
@@ -30,7 +24,8 @@ class DjangoApp(models.Model):
     is_hotclub = models.BooleanField(default=False, verbose_name=_("Is a Hotclub Application"))
     version = models.CharField(max_length=50, blank=True, verbose_name=_("Version"))
     download_url = models.URLField(verbose_name=_("Download URL"))
-    author = models.ForeignKey(Author)
+
+    users = models.ManyToManyField(User)
 
     def save(self):
         if not self.slug:
